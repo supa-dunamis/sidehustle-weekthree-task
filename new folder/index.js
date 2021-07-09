1a68258b013417a7d51da480364e8f0275524290
supa-dunamis <85125900+supa-dunamis@users.noreply.github.com>
2021-07-09 14:25:17 +0100

    Add files via upload

---
+++ new folder/index.js
@@ -0,0 +1,17 @@
+let counterDisplay = document.querySelector('.counterDisplay');
+let lowerCount = document.querySelector('.lower-count');
+let addCount = document.querySelector('.add-count');
+let number = 0;
+
+addCount.addEventListener('click' , () => {
+    if(number<50){
+        number ++;
+    }
+    counterDisplay.innerHTML=number;
+} );
+lowerCount.addEventListener('click' , () => {
+    if(number>0){
+        number --;
+    }
+    counterDisplay.innerHTML=number;
+} );
